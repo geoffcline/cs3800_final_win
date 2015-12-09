bc3f8cc97090e4d229d5e9d6e8b636747f640d59
Geoffrey Cline <geoffreyc@outlook.com>
2015-12-08 20:43:03 -0600

    init g

---
+++ knapsack3/knapsack3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  knapsack3
+//
+//  Created by Geoffrey on 12/8/15.
+//  Copyright © 2015 Geoffrey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
